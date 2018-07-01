1636a9b9f5757c7c4bf74e38b0b53d240b5889f6
Nathan Smith <nathan.smith5@mail.mcgill.ca>
2018-07-01 14:39:19 -0700

    better server

--- src/main.go
+++ src/main.go
@@ -1,26 +1,55 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
-	"log"
+	"fmt"
 	"net/http"
 	"time"
 )
+
+type interceptingWriter struct {
+	code int
+	http.ResponseWriter
+}
+
+func (iw *interceptingWriter) WriteHeader(code int) {
+	iw.code = code
+	iw.ResponseWriter.WriteHeader(code)
+}
+
+// Log is the structure for logging
+type Log struct {
+	Time     time.Time `json:"time"`
+	IP       string    `json:"ip"`
+	Method   string    `json:"method"`
+	Status   int       `json:"status"`
+	Duration string    `json:"duration"`
+	Path     string    `json:"path"`
+	Referrer string    `json:"referrer"`
+}
 
 // Logger is a logging middleware. Path, request time and Method are recorded
 func Logger(inner http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
+		iw := interceptingWriter{200, w}
 
-		inner.ServeHTTP(w, r)
+		inner.ServeHTTP(&iw, r)
 
-		log.Printf(
-			"%s\t%s\t%s\t%s",
-			r.Header.Get("X-Forwarded-For"),
-			r.Method,
-			r.RequestURI,
-			time.Since(start),
-		)
+		d := fmt.Sprintf("%s", time.Since(start))
+		l := Log{
+			Time:     time.Now(),
+			IP:       r.Header.Get("X-Forwarded-For"),
+			Method:   r.Method,
+			Status:   iw.code,
+			Duration: d,
+			Path:     r.RequestURI,
+			Referrer: r.Referer(),
+		}
+
+		b, _ := json.Marshal(l)
+		fmt.Println(string(b))
 	})
 }
 
@@ -32,7 +61,5 @@ func main() {
 	fs := http.FileServer(http.Dir(*dir))
 	fs = Logger(fs)
 	http.Handle("/", fs)
-
-	log.Println("Listening...")
 	http.ListenAndServe(":3000", nil)
 }
